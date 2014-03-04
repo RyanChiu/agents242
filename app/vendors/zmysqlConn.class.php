951a1c10847b92e932bdcce1dfc8ed8452dd34c2
RayAllen <ray@ray-Lenovo-IdeaPad-S400.(none)>
2014-03-04 22:00:16 +0800

    change "pddcake" to "pddcake242" in zmysqlConn.class.php
    Signed-off-by: RayAllen <ray@ray-Lenovo-IdeaPad-S400.(none)>

--- app/vendors/zmysqlConn.class.php
+++ app/vendors/zmysqlConn.class.php
@@ -11,7 +11,7 @@ class zmysqlConn
 	{
 		$this->dblink = mysql_connect($host, $username, $password)
 			or dir ("Something wrong with: " . mysql_error());
-		mysql_select_db("pddcake", $this->dblink)
+		mysql_select_db("pddcake242", $this->dblink)
 			or die ("Something wrong with: " . mysql_error());
 	}
 }
