06eece43d2186a03f0e9fe251c23dc115ec1d830
RayAllen <ray@ray-Lenovo-IdeaPad-S400.(none)>
2014-03-07 11:09:25 +0800

    !!!important, try to make the old link work with "trans" part.
    !!!important, try to make the old link work with "trans" part.
    
    Signed-off-by: RayAllen <ray@ray-Lenovo-IdeaPad-S400.(none)>

---
+++ app/Controller/TransController.php
@@ -0,0 +1,10 @@
+<?php
+class TransController extends AppController {
+	var $name = 'Accounts';
+	
+	function beforeFilter() {
+		$this->redirect(array("controller" => "accounts", "action" => "index"));
+	
+		parent::beforeFilter();
+	}
+}
